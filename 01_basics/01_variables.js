87be3d678247c490516d21f330a4e3363ec4e15a
AVIRAL MAHESHWARI <105984498+Aviral-Maheshwari@users.noreply.github.com>
2025-06-16 17:38:54 +0000

    missing file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "aviofficial28@gmail.com"
+var accountPassword = "1233"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId=2
+accountEmail = "hui.google.com"
+accountPassword = "23412"
+accountCity = "Bengalore"
+
+
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword, accountCity, accountState ]);
+
