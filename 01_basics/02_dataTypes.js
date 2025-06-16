4a9b370959baa9831f7996360dd174c082c00d57
AVIRAL MAHESHWARI <105984498+Aviral-Maheshwari@users.noreply.github.com>
2025-06-16 16:39:46 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+// alert("Hello") this would not work in here , as we are just using js not browser
+
+let name = "Stra"
+let age = 18
+let isLoggedIn = true
+
+
+// number => 2 to power 53
+// bigint 
+// string 
+// boolean = true / false
+// null this is a standalone value as well as data type , means it represent empty value
+// undefined , when value is not define
+// symbol, to find uniqueness between different components 
+
+// object {}
+
+console.log(typeof(undefined))
+
+
+
