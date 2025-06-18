f6c559212649d62db23e8b4456d6b4dfccc18924
AVIRAL MAHESHWARI <105984498+Aviral-Maheshwari@users.noreply.github.com>
2025-06-18 17:07:44 +0000

    obj completed

---
+++ 02_basics/05_Objects.js
@@ -0,0 +1,15 @@
+// Objects DeStructuring 
+
+const course = {
+    courseName:"JS",
+    Price:"999",
+    courseInstr:"Ramesh"
+}
+//Destructuring of objects
+const {courseInstr: instr} = course
+console.log(instr)
+
+// Basics of API's
+
+
+
